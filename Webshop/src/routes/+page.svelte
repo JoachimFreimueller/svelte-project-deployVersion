ea35c3db56cff5ef586868dd536d769dad6a80b5
Joachim Freimueller <FreimuellerJ21038@sus.htl-grieskirchen.at>
2025-05-16 12:36:41 +0200

    filter funtion implemented

--- Webshop/src/routes/+page.svelte
+++ Webshop/src/routes/+page.svelte
@@ -3,26 +3,45 @@
     import Product from "../Components/Product.svelte";
 
     let products =$state([]);
+    let filteredProducts = $state([]);
+    let searchText = $state("");
 
         onMount(async () => {
         let response = await fetch("https://fakestoreapi.com/products");
         products = await response.json();
-        let i = 0;
-
     });
 
+    function filterProducts()
+    {
+        filteredProducts = products.filter((value) => value.title.includes(searchText));
+    }
 
-  
 
 </script>
 
 
 <div class="p-8 bg-gray-50 min-h-screen">
-    <h1 class="text-3xl font-bold mb-6 text-gray-800">🛒 Unser Shop</h1>
+    <h1 class="text-3xl font-bold mb-6 text-gray-800">🛒 Shopfner</h1>
+
+    <input bind:value={searchText} onkeydown={() => filterProducts()}
+    type="text"
+    placeholder="Suche nach Produkten..."
+    class="w-full sm:w-1/2 px-4 py-3 rounded-xl border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition"
+  />
+  
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        
+        {#if searchText == false}
+
         {#each products as product}
         <Product id = {product.id} title = {product.title} price = {product.price} description = {product.description} imageSource = {product.image} rating = {product.rating} category = {product.category}></Product>
-    {/each} 
+        {/each} 
+
+        {:else}
+        {#each filteredProducts as product}
+        <Product id = {product.id} title = {product.title} price = {product.price} description = {product.description} imageSource = {product.image} rating = {product.rating} category = {product.category}></Product>
+        {/each}         
+        {/if}
     
     </div>
 </div>
