7c7ce885aed1918e35c6a65f9b7c8da73c548a4d
Joachim Freimueller <FreimuellerJ21038@sus.htl-grieskirchen.at>
2025-06-06 11:26:01 +0200

    New Page added to enable routing

--- Webshop/src/Components/Product.svelte
+++ Webshop/src/Components/Product.svelte
@@ -1,11 +1,15 @@
 <script>
 
     let {id, title, price, description, imageSource, rating, category} = $props();
+    function showDetails()
+    {
+        //route switch
+    }
 
 </script>
 
 
-<div class="max-w-sm rounded-2xl shadow-lg overflow-hidden bg-white hover:shadow-xl transition-shadow duration-300 m-4 cursor-pointer">
+<div onclick={() => showDetails()} class="max-w-sm rounded-2xl shadow-lg overflow-hidden bg-white hover:shadow-xl transition-shadow duration-300 m-4 cursor-pointer">
     <img class="w-full h-48 object-contain p-4" src={imageSource} alt={title} />
     <div class="p-4 flex flex-col justify-between">
         <h2 class="text-xl font-semibold text-gray-800 mb-2 truncate">{title}</h2>
